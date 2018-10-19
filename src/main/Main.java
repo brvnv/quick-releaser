872f95d5ac84a364a2306a453edc2014fde587db
brvnv <bruno.rdx@metalfan.com>
2018-10-19 10:58:19 -0300

    Initial commit, or "time to fix the compilation errors"

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,5 +12,22 @@ public class Main {
 		FileReader file=new FileReader(sc);
 		BufferedReader buf=new BufferedReader(file);
 		System.out.println("Processing...");
+		String process=buf.readLine();
+		while(process!=null) { // do while line is not finished
+			if(process!matches("[")) addsRel();
+			else addsCat();
+		}
+		System.out.println("Done. You're welcome.");
+		file.close();
 	}
+}
+
+public static void addsRel() {
+	String $relname=process;
+	System.out.println("+addrel" + $cat + " " + relname);
+}
+
+public static void addsCat() {
+	String $cat=process.substring(1,(process.length());
+}
 }
